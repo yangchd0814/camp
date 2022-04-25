5e3190d89288a759bc7bdd5a4d9f1ae6eafaf2dc
yangchd <404521296@qq.com>
2022-04-25 22:02:42 +0800

    project init

---
+++ src/test/java/com/yangchd/camp/CampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangchd.camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
