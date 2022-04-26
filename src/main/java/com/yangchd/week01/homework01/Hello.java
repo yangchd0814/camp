82c5d85b82c3f233543b0e5503be3aa1262645a1
yangchd <404521296@qq.com>
2022-04-26 22:23:16 +0800

    作业1和作业3

---
+++ src/main/java/com/yangchd/week01/homework01/Hello.java
@@ -0,0 +1,24 @@
+package com.yangchd.week01.homework01;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a = 1;
+        float b = 2;
+        double c = 3;
+        boolean d = true;
+        long e = 4;
+        char f = 'f';
+        byte g = 10;
+        short h = 2;
+        a = a + 2;
+        a = a - 1;
+        a = 4 * 4;
+        a = 10/2;
+        if (a == 1) {
+            a = 2 + 3;
+        }
+        for (int i = 0; i < 6; i++) {
+            a = a + i;
+        }
+    }
+}
